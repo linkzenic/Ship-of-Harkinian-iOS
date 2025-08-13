231861d2c7fa9aa405bc2b142e0d2a27b0145230
PurpleHato <linkvssangoku.jr@gmail.com>
2025-08-13 04:57:19 +0200

    TWEAK: SoT Time Travel - Allow Swordless (#5716)
    * Magic number + more modular
    
    * tweak + switch
    
    * forgot basic oot
    
    * linebreak
    
    * c-lang

--- soh/soh/Enhancements/mods.cpp
+++ soh/soh/Enhancements/mods.cpp
@@ -130,10 +130,27 @@ void RegisterOcarinaTimeTravel() {
         bool notNearAnySource = !nearbyTimeBlockEmpty && !nearbyTimeBlock && !nearbyOcarinaSpot && !nearbyDoorOfTime &&
                                 !nearbyFrogs && !nearbyGossipStone;
         bool hasOcarinaOfTime = (INV_CONTENT(ITEM_OCARINA_TIME) == ITEM_OCARINA_TIME);
-        bool doesntNeedOcarinaOfTime = CVarGetInteger(CVAR_ENHANCEMENT("TimeTravel"), 0) == 2;
         bool hasMasterSword = CHECK_OWNED_EQUIP(EQUIP_TYPE_SWORD, EQUIP_INV_SWORD_MASTER);
-        // TODO: Once Swordless Adult is fixed: Remove the Master Sword check
-        if (justPlayedSoT && notNearAnySource && (hasOcarinaOfTime || doesntNeedOcarinaOfTime) && hasMasterSword) {
+        int timeTravelSetting = CVarGetInteger(CVAR_ENHANCEMENT("TimeTravel"), 0);
+        bool meetsTimeTravelRequirements = false;
+
+        switch (timeTravelSetting) {
+            case TIME_TRAVEL_ANY:
+                meetsTimeTravelRequirements = true;
+                break;
+            case TIME_TRAVEL_ANY_MS:
+                meetsTimeTravelRequirements = hasMasterSword;
+                break;
+            case TIME_TRAVEL_OOT_MS:
+                meetsTimeTravelRequirements = hasMasterSword && hasOcarinaOfTime;
+                break;
+            case TIME_TRAVEL_OOT:
+            default:
+                meetsTimeTravelRequirements = hasOcarinaOfTime;
+                break;
+        }
+
+        if (justPlayedSoT && notNearAnySource && meetsTimeTravelRequirements) {
             SwitchAge();
         }
     });
